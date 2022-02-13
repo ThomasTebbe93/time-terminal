cfff9ed85d9d17768c217e0d4f7d26fbdf80bbed
Thomas Tebbe <thomas.tebbe@hotmail.de>
2022-02-13 13:27:34 +0100

    clear pins

--- ChipReader.py
+++ ChipReader.py
@@ -14,23 +14,26 @@ class ChipReader(object):
         )
 
     def intToHex(self, value: int) -> string:
-        return f'{value:x}'
+        return f"{value:x}"
 
     def read(self) -> string:
-        MIFAREReader = mfrc.MFRC522()
-        (status) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        try:
+            MIFAREReader = mfrc.MFRC522()
+            (status) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-        if status == MIFAREReader.MI_OK:
-            print("Card detected")
+            if status == MIFAREReader.MI_OK:
+                print("Card detected")
 
-        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
-        if status == MIFAREReader.MI_OK:
-            print(
-                "Card read UID: %s,%s,%s,%s"
-                % (hex(uid[0]), hex(uid[1]), hex(uid[2]), hex(uid[3]))
-            )
-            uidList = [uid[0], uid[1], uid[2], uid[3]]
-            result = self.uidToString(uidList)
-            print("Card read UID: %s" % (result))
-            return result
+            if status == MIFAREReader.MI_OK:
+                print(
+                    "Card read UID: %s,%s,%s,%s"
+                    % (hex(uid[0]), hex(uid[1]), hex(uid[2]), hex(uid[3]))
+                )
+                uidList = [uid[0], uid[1], uid[2], uid[3]]
+                result = self.uidToString(uidList)
+                print("Card read UID: %s" % (result))
+                return result
+        finally:
+            GPIO.cleanup()
--- README.md
+++ README.md
@@ -1,5 +1,66 @@
 # time-terminal
 
-## Install
+## hardware
 
-pip install requests
+Raspberry pi 4B
+sd-card
+RFID-Module with chips (https://www.amazon.de/gp/product/B00QFDRPZY?ie=UTF8&linkCode=as2&camp=1634&creative=6738&tag=754-21&creativeASIN=B00QFDRPZY)
+Display (https://www.amazon.de/gp/product/B08CH24YYD/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1)
+some jumpercables
+
+## Connect to raspberry pi
+
+SDA -> Pin 24 / GPIO8 (CE0)
+SCK -> Pin 23 / GPIO11 (SCKL)
+MOSI -> Pin 19 / GPIO10 (MOSI)
+MISO -> Pin 21 / GPIO9 (MISO)
+IRQ —
+GND -> Pin6 (GND)
+RST -> Pin22 / GPIO25
+3.3V -> Pin 1 (3V3)
+
+## Prepare raspbery pi
+
+### Configure boot config
+
+sudo nano /boot/config.txt
+add
+device_tree_param=spi=on
+dtoverlay=spi-bcm2708
+save and quit (STRG+O, STRG+X)
+
+### Aktivate SPI
+
+sudo raspi-config
+„Advanced Options“ > „SPI“ activate
+reboot pi
+
+### Install packages
+
+#### Update raspberry pi
+
+sudo apt update
+
+#### Insatll python
+
+sudo apt install python3-dev python3-pip
+
+#### Install git
+
+sudo apt-get install git
+
+#### Install lib for gui
+
+sudo apt-get install python3-tk
+
+#### Install SPI Module
+
+sudo pip3 install spidev
+
+#### Insatll lib for RFID
+
+sudo pip3 install mfrc522
+
+#### Install lib for http-requests
+
+sudo pip3 install requests
